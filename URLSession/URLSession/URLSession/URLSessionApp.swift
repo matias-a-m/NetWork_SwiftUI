bdb14d02b7162db887b0fa9a76c30959b789ad57
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2023-12-07 14:40:42 -0300

    added urlsession project

---
+++ URLSession/URLSession/URLSession/URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionApp.swift
+//  URLSession
+//
+//  Created by matias on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
