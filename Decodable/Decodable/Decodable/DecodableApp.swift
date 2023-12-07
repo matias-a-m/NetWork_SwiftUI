9814e212900ff02240ee60fbb72fbe95ca0ec420
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2023-12-07 15:53:13 -0300

    added project Decodable

---
+++ Decodable/Decodable/Decodable/DecodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecodableApp.swift
+//  Decodable
+//
+//  Created by matias on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DecodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
