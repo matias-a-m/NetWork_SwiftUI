ae912bc39582204433030981faa44835fa06f478
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2023-12-10 18:51:27 -0300

    added HTTPS_request porject

---
+++ HTTPS_request/HTTPS_request/HTTPS_request/HTTPS_requestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPS_requestApp.swift
+//  HTTPS_request
+//
+//  Created by matias on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct HTTPS_requestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
