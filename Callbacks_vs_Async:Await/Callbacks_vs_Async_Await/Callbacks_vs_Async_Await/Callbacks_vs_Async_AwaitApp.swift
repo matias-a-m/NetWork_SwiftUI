e9bd6ac83da92aa817061724ca6b0d7553ade7d4
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2023-12-11 20:06:29 -0300

    added Callsbacks_vs_Async_Await_multiple_request

---
+++ Callbacks_vs_Async:Await/Callbacks_vs_Async_Await/Callbacks_vs_Async_Await/Callbacks_vs_Async_AwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Callbacks_vs_Async_AwaitApp.swift
+//  Callbacks_vs_Async_Await
+//
+//  Created by matias on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Callbacks_vs_Async_AwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
